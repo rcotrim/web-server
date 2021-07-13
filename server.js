edd7bcc72c2e1a14920592cd964ceac4afc54574
Richard Cotrim <richard.cotrim@rcotrim.com.br>
2021-07-13 11:18:11 -0300

    Separate middleware function to proper file

--- server.js
+++ server.js
@@ -2,16 +2,8 @@ var express = require('express');
 var app = express();
 var PORT = 3000;
 
-var middleware = {
-    requireAuthentication: function (req, res, next) {
-        console.log('private route hit !');
-        next();
-    },
-    logger: function (req, res, next) {
-        console.log(new Date().toString() + ' - Request: ' + req.method + '  ' + req.originalUrl);
-        next();
-    }
-}
+var middleware = require('./middleware')
+
 
 app.use(middleware.logger);
 
